5f3b9fc1a15321a0c5b0cb985feb434dbebba9e6
CodeHart-art <jobhart11211@gmail.com>
2026-04-07 11:21:15 -0400

    Asked user for equation components and formed mortgage equation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,28 @@ public class Main {
         System.out.print("Select Calculator(A,B,C): ");
         String calculator = scanner.nextLine();
 
-        if (calculator.equalsIgnoreCase("a"))
-            System.out.println("it works");
 
 
+        if (calculator.equalsIgnoreCase("a")){
+            System.out.println("welcome to the mortgage calculator");
 
+            System.out.print("What is your principle amount: ");
+            double princible = scanner.nextDouble();
+
+            System.out.print("What is your annual interest rate: ");
+            double annualInterestRate = scanner.nextDouble();
+
+            System.out.print("How many years is your loan: ");
+            int loanLength = scanner.nextInt();
+
+            double monthlyInterest = (annualInterestRate/100) / 12;
+            int totalMonths = loanLength * 12;
+
+
+            double monthlyPayments = princible * (monthlyInterest * Math.pow(1 + monthlyInterest,totalMonths)) / (Math.pow(1 + monthlyInterest,totalMonths) - 1);
+
+            System.out.println("Here are your monthly payments: $ " + monthlyPayments);
+        }
         scanner.close();
     }
 }

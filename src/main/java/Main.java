fc3679d4cd2d701a2254068e4ab8989fd240420b
CodeHart-art <jobhart11211@gmail.com>
2026-04-07 09:24:13 -0400

    scanner functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Select Calculator(A,B,C): ");
+        String calculator = scanner.nextLine();
+
+        if (calculator.equalsIgnoreCase("a"))
+            System.out.println("it works");
+
+
+
+        scanner.close();
     }
 }
